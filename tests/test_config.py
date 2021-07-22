4eae66a53851cf7a836d58784d5bc261eb33b827
venkatareddyjillela <64194665+venkatareddyjillela@users.noreply.github.com>
2021-07-22 16:42:00 +0530

    web structure is added

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,7 +5,12 @@ class NotInRange(Exception):
         self.message = message
         super().__init__(self.message)
 def test_generic():
-    a = 15
+    a = 1
     with pytest.raises(NotInRange):
         if a not in range(10,20):
             raise NotInRange
+
+def test_something():
+    a = 2
+    b = 2
+    assert True
