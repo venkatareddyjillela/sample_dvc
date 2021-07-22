d4b675be288556f38bd52be4d154dc88f4817ff0
venkatareddyjillela <64194665+venkatareddyjillela@users.noreply.github.com>
2021-07-22 13:06:23 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name = "src",
+    version = "1.0.0",
+    description = "wine quality package",
+    author = "venkatareddyjillela",
+    packages = find_packages(),
+    license = "MIT"
+)
