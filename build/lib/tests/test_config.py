3c47241303d2a5bb3e93d613293ed3509637ed20
venkatareddyjillela <64194665+venkatareddyjillela@users.noreply.github.com>
2021-07-23 13:33:05 +0530

    update tox file

---
+++ build/lib/tests/test_config.py
@@ -0,0 +1,16 @@
+import pytest 
+
+class NotInRange(Exception):
+    def __init__(self,message="value is not in range"):
+        self.message = message
+        super().__init__(self.message)
+def test_generic():
+    a = 1
+    with pytest.raises(NotInRange):
+        if a not in range(10,20):
+            raise NotInRange
+
+def test_something():
+    a = 2
+    b = 2
+    assert True
